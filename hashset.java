7e87a3440430a4b7fa9bffb525d761e187c0c57e
Arshad Shaikh <arshadshaikh53770@gmail.com>
2024-12-17 22:46:14 -0800

    Update hashset.java

--- hashset.java
+++ hashset.java
@@ -2,9 +2,12 @@
 import java.util.*;
 public class hashset{
     public static void main(String [] args){
-    HashSet<Integer> set = new HashSet<>(Arrays.asList(3,4,8,9,6,2));
+    HashSet<Integer> set = new HashSet<>(Arrays.asList(34,45,81,19,16,2));
         int max = Collections.max(set);
+        int min = Collections.min(set);
         System.out.println("Highet entry:" +max);
+        System.out.println("Minimum entry:" +min);
+        
 
     }
 }
